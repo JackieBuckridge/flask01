fe3230a4536e6e14c62584a42343015f93eae9a1
Jackie Buckridge <144824212+JackieBuckridge@users.noreply.github.com>
2023-12-20 13:13:41 +0000

    Added statice files and some folders

--- app.py
+++ app.py
@@ -1,10 +1,33 @@
-from flask import Flask
+import os
+from flask import Flask, render_template, request,send_from_directory
 
 app = Flask(__name__)
+UPLOAD_FOLDER = 'uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-if __name__=="__main__":
-    app.run(host='0.0.0.0')
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'video' not in request.files:
+        return "No video part"
+
+    file = request.files['video']
+
+    if file.filename == '':
+        return "No selected video file"
+
+    if file:
+        filename = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+        file.save(filename)
+        return 'Video uploaded successfully'
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+
+if __name__ == '__main__':
+    app.run(debug=True,host='0.0.0.0')

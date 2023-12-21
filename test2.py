c732f867bb62e919334dca58af6334564527465b
Jackie Buckridge <144824212+JackieBuckridge@users.noreply.github.com>
2023-12-21 15:28:51 +0000

    done working test2.py

--- test2.py
+++ test2.py
@@ -1,34 +1,28 @@
-import mmcv
-
 from mmaction2.mmaction.apis import inference_recognizer ,init_recognizer
+from mmengine import Config
+from operator import itemgetter
 
-config_path = 'check_poinst2/tsn_imagenet-pretrained-r50_8xb32-1x1x3-100e_kinetics400-rgb.py'
-checkpoint_path = 'check_poinst2/tsn_imagenet-pretrained-r50_8xb32-1x1x3-100e_kinetics400-rgb_20220906-cd10898e.pth' # can be a local path
-img_path = 'demo.mp4'   # you can specify your own picture path
+config = 'mmaction2/configs/recognition/tsn/tsn_imagenet-pretrained-r50_8xb32-1x1x3-100e_kinetics400-rgb.py'
+config = Config.fromfile(config)
+
+checkpoint_path="checkpoints/tsn_r50_1x1x3_100e_kinetics400_rgb_20200614-e508be42.pth"
+video = 'demo.mp4'   # you can specify your own picture path
+
+label="mmaction2/tools/data/kinetics/label_map_k400.txt"
 
 
-model=init_recognizer(config_path,checkpoint_path,device='cpu')
-
-video=mmcv.VideoReader(img_path)
+model=init_recognizer(config,checkpoint_path,device='cpu')
 
 
-frame_rate=video.fps
+results = inference_recognizer(model, video)
 
-total_frames=len(video)
+pred_scores = results.pred_score.tolist()
+score_tuples = tuple(zip(range(len(pred_scores)), pred_scores))
+score_sorted = sorted(score_tuples, key=itemgetter(1), reverse=True)
+top5_label = score_sorted[:5]
 
-result=inference_recognizer(model,img_path)
+labels = open(label).readlines()
+labels = [x.strip() for x in labels]
+results = [(labels[k[0]], k[1]) for k in top5_label]
 
-print(result)
-
-# Output the results
-for i, (label, score) in enumerate(zip(result['label'], result['score'])):
-    print(f'Frame {i+1}: Label - {label}, Score - {score}')
-
-# Load the label map
-with open("mmaction2/tools/data/kinetics/label_map_k400.txt", "r") as f:
-    label_map = [line.strip() for line in f]
-
-# Map labels to class names
-class_names = [label_map[label] for label in result['label']]
-
-print("Class Names:", class_names)
+print(results)
